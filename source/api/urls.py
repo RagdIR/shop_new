d6e77f926b71e427ff52b09c25bc25432584c023
RagdIR <ragdirstrogov@gmail.com>
2020-10-22 16:46:31 +0600

    Обновлена вьюшка, сделаны фикстуры

---
+++ source/api/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from api.views import ProductViewSet, OrderViewSet, get_token_view
+
+app_name = 'api'
+
+router = DefaultRouter()
+router.register(r'product', ProductViewSet)
+router.register(r'order', OrderViewSet)
+
+urlpatterns = [
+    path('get-token/', get_token_view, name='get_token'),
+    path('', include(router.urls))
+]
--- source/api/views.py
+++ source/api/views.py
@@ -2,8 +2,8 @@ from django.http import HttpResponse, HttpResponseNotAllowed
 from django.shortcuts import render, get_object_or_404
 from django.views.decorators.csrf import ensure_csrf_cookie
 from rest_framework.response import Response
-from rest_framework.viewsets import ViewSet
-
+from rest_framework.viewsets import ViewSet, ModelViewSet
+from rest_framework.permissions import SAFE_METHODS, IsAdminUser
 from api.serializers import ProductSerializer, OrderSerializer
 from webapp.models import Product, Order
 
@@ -14,7 +14,11 @@ def get_token_view(request, *args, **kwargs):
         return HttpResponse()
     return HttpResponseNotAllowed('Only GET request are allowed')
 
+
+
+
 class ProductViewSet(ViewSet):
+    # serializer class = ProductSerializer
     queryset = Product.objects.all()
 
     def list(self, request):
@@ -45,18 +49,19 @@ class ProductViewSet(ViewSet):
         product.delete()
         return Response({'pk':pk})
 
-
-class OrderViewSet(ViewSet):
-    def list(self, request):
-        objects = Order.objects.all()
-        slr = OrderSerializer(objects, many=True, context={'request':request})
-        return Response(slr.data)
+    def get_permissions(self):
+        if self.action == 'create' or 'update' or 'delete':
+            permission_classes = [IsAdminUser]
+        return [permission() for permission in permission_classes]
 
 
-    def create(self, request):
-        slr = OrderSerializer(data=request.data, context={'request':request})
-        if slr.is_valid():
-            order= slr.save()
-            return Response(slr.data)
-        else:
-            return Response(slr.errors, status=400)
+
+class OrderViewSet(ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+    def get_permissions(self):
+        if self.action == 'list':
+            permission_classes = [IsAdminUser]
+        return [permission() for permission in permission_classes]
+
